e36ae17857c29174e66f26145c840b8203721e26
CarlosDevit <carlosdevitsantos@gmail.com>
2023-06-03 14:40:46 -0300

    Mini-Loja-Virtual Exemplo

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,67 @@
-window.onload = function(){
-    var nome = "Devitm";
+const itens = [
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: 'images/image.jpg',
+        quantidade: 0
+    },
 
-    if(nome == "Devit"){
-        alert("Verdadeiro\n" + "Olá " + nome);
+    {
+        id: 1,
+        nome: 'short',
+        img: 'images/image.jpg',
+        quantidade: 0
+    },
+
+    {
+        id: 2,
+        nome: 'sapato',
+        img: 'images/image.jpg',
+        quantidade: 0
     }
-    else if (nome != "Devitm"){
-        nome = "devit";
-        alert("Falso\n" + "Olá Visitante!" + "Onde está o " + nome);
-    }
-    else{
-        alert("intruso");
-    }
+]
+
+inicializarLoja = () => {
+    var containerProdutos = document.getElementById('produtos');
+    itens.map((val) => {
+        containerProdutos.innerHTML += `
+        
+        <div class="produto-single">
+            <img src="`+ val.img + `">
+            <p>`+ val.nome + `</p>
+            <a key="`+ val.id + `" href="#">Adicoinar ao Carrinho</a>
+        </div>        
+        `;
+
+    })
+}
+
+inicializarLoja();
+
+atualizarCarrinho = () => {
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
+    itens.map((val) => {
+        if (val.quantidade > 0) {
+            containerCarrinho.innerHTML += `
+
+            <p>`+ val.nome + ` | quantidade: ` + val.quantidade + `</p>
+            <hr>
+            
+            
+        `;
+        }
+    })
+
+}
+
+var links = document.getElementsByTagName('a');
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function () {
+        let key = this.getAttribute('key');
+        itens[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+    })
 }
